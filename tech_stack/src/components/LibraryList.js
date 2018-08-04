de51df02a23af6419ae4c0ed974547353936cff9
leolleocomp <leolleo.comp@gmail.com>
2018-08-04 16:55:22 -0300

    Add last created files

---
+++ tech_stack/src/components/LibraryList.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { FlatList } from 'react-native';
+import ListItem from './ListItem';
+
+class LibraryList extends Component {
+  renderItem(library) {
+    return (
+      <ListItem library={library.item} />
+    );
+  }
+
+  render() {
+    return <FlatList 
+      data={this.props.libraries}
+      renderItem={this.renderItem.bind(this)}
+      keyExtractor={library => library.id}
+    />;
+  }
+}
+
+const mapStateToProps = ({ libraries }) => ({ libraries });
+
+export default connect(mapStateToProps)(LibraryList);
