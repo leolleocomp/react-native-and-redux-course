b9b96d0ff992fdedbccc205df650eb405353b072
leolleocomp <leolleo.comp@gmail.com>
2018-08-14 20:11:13 -0300

    Finishes the section "Dont mutate that state"

--- manager/src/components/LoginForm.js
+++ manager/src/components/LoginForm.js
@@ -1,12 +1,54 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { emailChanged }  from './actions';
-import { Card, CardSection, Input, Button } from './common';
+import { View, Text } from 'react-native';
+import { emailChanged, passwordChanged, loginUser }  from '../actions';
+import { Card, CardSection, Input, Button, Spinner } from './common';
 
+const styles = {
+    errorTextStyle: {
+        fontSize: 20,
+        alignSelf: 'center',
+        color: 'red',
+    },
+};
 
 class LoginForm extends Component {
     onEmailChange(text) {
         this.props.emailChanged(text);
+    }
+
+    onPasswordChange(text) {
+        this.props.passwordChanged(text);
+    }
+
+    onPressButton() {
+        const { email, password } = this.props;
+
+        this.props.loginUser({ email, password });
+    }
+
+    renderError() {
+        if (this.props.error) {
+            return (
+                <View style={{ backgroundColor: 'white' }}>
+                    <Text style={styles.errorTextStyle}>
+                        {this.props.error}
+                    </Text>
+                </View>
+            )
+        }
+    }
+    
+    renderButton() {
+        if (this.props.loading) {
+            return <Spinner size="large" />;
+        }
+
+        return (
+            <Button onPress={this.onPressButton.bind(this)}>
+                Login
+            </Button>
+        );
     }
 
     render() {
@@ -17,6 +59,7 @@ class LoginForm extends Component {
                         label="Email"
                         placeholder="email@email.com"
                         onChangeText={this.onEmailChange.bind(this)}
+                        value={this.props.email}
                     />
                 </CardSection>            
                 
@@ -25,17 +68,30 @@ class LoginForm extends Component {
                         secureTextEntry
                         label="Password"
                         placeholder="password"
+                        onChangeText={this.onPasswordChange.bind(this)}
+                        value={this.props.password}
                     />
                 </CardSection>
                 
+                { this.renderError() }
+
                 <CardSection>
-                    <Button>
-                        Login
-                    </Button>
+                    { this.renderButton() }
                 </CardSection>
             </Card>
         );
     }
 }
 
-export default connect(null, emailChanged)(LoginForm);
+const mapStateToProps = state => {
+    return {
+        email: state.auth.email,
+        password: state.auth.password,
+        error: state.auth.error,
+        loading: state.auth.loading,
+    };
+};
+
+export default connect(mapStateToProps, {
+    emailChanged, passwordChanged, loginUser 
+})(LoginForm);
